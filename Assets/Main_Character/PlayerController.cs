44947f2ec454335a3ef612b537f8e550afe5a7c0
nanakon <nanakon@192.168.2.102>
2025-10-23 23:21:14 +0800

    u

---
+++ Assets/Main_Character/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed;
+    private Rigidbody2D myRigidbody;
+    private Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Filp();
+    }
+
+    void Filp()
+    {
+        bool playerHasXAxisSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasXAxisSpeed)
+        {
+            if (myRigidbody.velocity.x > 0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+            if (myRigidbody.velocity.x < -0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+
+            }
+        }
+
+    }
+
+    void Run()
+    {
+        float moveDir = Input.GetAxis("Horizontal");
+        Vector2 playerVel = new Vector2(moveDir * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVel;
+
+        // idle与run的动画切换
+        // x轴速度大于一个极小值
+        bool playerHasXAxisSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnim.SetBool("Run", playerHasXAxisSpeed);
+    }
+}
